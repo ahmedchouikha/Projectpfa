a857720e25566007d7af481651fd237feb7f0d8e
ahmedchouikha <73880842+ahmedchouikha@users.noreply.github.com>
2020-11-03 13:19:38 +0100

    jump obstacle

--- delete-this-project/Assets/PlayerMovement.cs
+++ delete-this-project/Assets/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     public int velocity1;
     public int velocity2;
+    public int jump;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +26,10 @@ public class PlayerMovement : MonoBehaviour
 
         {
             rb.AddForce(-velocity2 * Time.deltaTime, 0, 0,ForceMode.VelocityChange );
+        }
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, jump, 0);
         }
     }
 }

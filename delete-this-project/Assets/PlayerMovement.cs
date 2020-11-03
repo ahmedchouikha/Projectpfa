177028d6866f04ee4e6855a29f80e139f10d09bb
ahmedchouikha <73880842+ahmedchouikha@users.noreply.github.com>
2020-11-03 11:44:36 +0100

    player movement

---
+++ delete-this-project/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int velocity1;
+    public int velocity2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(velocity1 * Time.deltaTime, 0, 0);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, velocity2 * Time.deltaTime);
+        }
+        if (Input.GetKey("q"))
+        {
+            rb.AddForce(0, 0, velocity2 * Time.deltaTime);
+        }
+    }
+}
